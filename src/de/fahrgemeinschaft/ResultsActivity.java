c40251193ca59f9934c3c25c60cc3f803b17dc28
orangeman <flo@teleportr.org>
2013-05-08 22:15:26 +0200

    contact chooser

--- src/de/fahrgemeinschaft/ResultsActivity.java
+++ src/de/fahrgemeinschaft/ResultsActivity.java
@@ -7,12 +7,22 @@
 
 package de.fahrgemeinschaft;
 
+import java.util.ArrayList;
+
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Intents.Insert;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
@@ -89,28 +99,88 @@ public class ResultsActivity extends SherlockFragmentActivity
             .addToBackStack(null).commit();
     }
 
-    public void call(View v) {
-        Cursor c = ((CursorAdapter) list.getListAdapter()).getCursor();
-        c.moveToPosition(selected);
-        String[] split = c.getString(7).split("; ");
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].startsWith("mobile")) {
-                split = split[i].split("=");
-                startActivity(new Intent(Intent.ACTION_CALL,
-                        Uri.parse("tel:" + split[1])));
-                return;
-            }
-            if (split[i].startsWith("landline")) {
-                split = split[i].split("=");
-                startActivity(new Intent(Intent.ACTION_CALL,
-                        Uri.parse("tel:" + split[1])));
-                return;
-            }
-        }
-        Log.d(TAG, "no phone number");
-    }
     @Override
     public void onPageSelected(int position) {
+        selected = position;
+    }
+
+    public void contact(View v) {
+        Cursor c = ((CursorAdapter) list.getListAdapter()).getCursor();
+        c.moveToPosition(selected);
+        Intent contact = new Intent(Intent.ACTION_INSERT, Contacts.CONTENT_URI);
+        contact.putExtra(Insert.NAME, c.getString(1) + " -> " + c.getString(3));
+        contact.putExtra(ContactsContract.Intents.EXTRA_FORCE_CREATE, true);
+        ArrayList<Intent> intents = new ArrayList<Intent>();
+        String[] who = c.getString(7).split("; ");
+        for (int i = 0; i < who.length; i++) {
+            String[] split = who[i].split("=");
+            if (split.length > 1) {
+                if (split[0].equals("mobile")) {
+                    System.out.println(split[1]);
+                    contact.putExtra(Insert.PHONE, split[1]);
+                    Intent call = labeledIntent(callIntent(split[1]),
+                            R.drawable.ic_call, "Call " + split[1]);
+                    if (call != null) intents.add(call);
+                    Intent sms = labeledIntent(smsIntent(split[1],
+                            c.getString(1) + " -> " + c.getString(3)),
+                            R.drawable.ic_sms, "SMS " + split[1]);
+                    if (sms != null) intents.add(sms);
+                } else if (split[0].equals("landline")) {
+                    contact.putExtra(Insert.SECONDARY_PHONE, split[1]);
+                    Intent call = labeledIntent(callIntent(split[1]),
+                            R.drawable.ic_dial, "Call " + split[1]);
+                    if (call != null) intents.add(call);
+                } else if (split[0].equals("mail")) {
+                    contact.putExtra(Insert.EMAIL, split[1]);
+                    Intent mail = labeledIntent(mailIntent(split[1],
+                            c.getString(1) + " --> " + c.getString(3)),
+                            R.drawable.ic_mail, "Mail " + split[1]);
+                    if (mail != null) intents.add(mail);
+                }
+            }
+        }
+        startActivity(Intent.createChooser(contact, "Kontakt")
+                .putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                intents.toArray(new Parcelable[intents.size()])));
+    }
+
+    private Intent callIntent(String num) {
+        Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + num));
+        return call;
+    }
+
+    private Intent smsIntent(String num, String text) {
+        Intent sms = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:" + num));
+        sms.putExtra("sms_body", text + "\n noch Platz?");
+        sms.addCategory(Intent.CATEGORY_DEFAULT);
+//        sms.setType("vnd.android-dir/mms-sms");
+        return sms;
+    }
+
+    private Intent mailIntent(String a, String text) {
+        Intent mail = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"+ a));
+        mail.putExtra(Intent.EXTRA_TEXT, text + "\n noch Platz frei?");
+        mail.putExtra(Intent.EXTRA_SUBJECT, "Fahrgemeinschaft");
+//        mail.setType("plain/text");
+        return mail;
+    }
+
+    public LabeledIntent labeledIntent(Intent intent, int icon, String label) {
+        System.out.println("resolving " + label);
+        PackageManager pm = getPackageManager();
+        ComponentName cmp = intent.resolveActivity(pm);
+        if (cmp != null) {
+            System.out.println("found " + cmp);
+            intent.setComponent(cmp);
+            Intent resolved = new Intent();
+            resolved.setData(intent.getData());
+            resolved.setComponent(cmp);
+            resolved.setAction(intent.getAction());
+            if (intent.getExtras() != null)
+                resolved.putExtras(intent.getExtras());
+            return new LabeledIntent(intent, getPackageName(), label, icon);
+        }
+        return null;
     }
 
     @Override
