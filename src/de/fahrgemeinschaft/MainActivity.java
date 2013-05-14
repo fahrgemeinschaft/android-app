7bc60c79cebc192828552aeb571fffd9e55efa7c
orangeman <flo@teleportr.org>
2013-05-14 21:01:45 +0200

    compiles

--- src/de/fahrgemeinschaft/MainActivity.java
+++ src/de/fahrgemeinschaft/MainActivity.java
@@ -7,17 +7,21 @@
 
 package de.fahrgemeinschaft;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.teleportr.ConnectorService;
 import org.teleportr.Ride;
 
-import android.app.DialogFragment;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -126,24 +130,24 @@ public class MainActivity extends SherlockActivity implements OnClickListener {
         }
     }
 
-    public static class TimePickerFragment extends DialogFragment
-    implements TimePickerDialog.OnTimeSetListener {
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
 
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    		// Use the current time as the default values for the picker
-    		final Calendar c = Calendar.getInstance();
-    		int hour = c.get(Calendar.HOUR_OF_DAY);
-    		int minute = c.get(Calendar.MINUTE);
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
 
-    		// Create a new instance of TimePickerDialog and return it
-    		return new TimePickerDialog(getActivity(), this, hour, minute,
-    				DateFormat.is24HourFormat(getActivity()));
-    	}
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
 
-    	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-    		// Do something with the time chosen by the user
-    	}
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
     }
     
     @Override
