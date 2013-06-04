35f92b7111a6234c6b54f22b86b74e7f0d278f8d
orangeman <flo@teleportr.org>
2013-06-04 19:24:16 +0200

    textsize in about

--- src/de/fahrgemeinschaft/AboutFragment.java
+++ src/de/fahrgemeinschaft/AboutFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
