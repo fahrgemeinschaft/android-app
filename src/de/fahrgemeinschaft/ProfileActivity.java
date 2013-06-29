68199bbd5f690cc99bb36eb45a5e197d1a7ed8e4
MoritzC <moritz@sonnenstreifen.de>
2013-06-29 18:27:55 +0200

    profile screen

---
+++ src/de/fahrgemeinschaft/ProfileActivity.java
@@ -0,0 +1,58 @@
+/**
+ * Fahrgemeinschaft / Ridesharing App
+ * Copyright (c) 2013 by it's authors.
+ * Some rights reserved. See LICENSE.. 
+ *
+ */
+
+package de.fahrgemeinschaft;
+
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+public class ProfileActivity extends SherlockPreferenceActivity {
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.profile);
+
+        final EditTextPreference user = (EditTextPreference)
+                findPreference("username");
+        user.setOnPreferenceChangeListener(
+                new OnPreferenceChangeListener() {
+                    
+                    @Override
+                    public boolean onPreferenceChange(Preference p, Object o) {
+                        p.setSummary(o.toString());
+                        return true;
+                    }
+                });
+        user.setSummary(user.getText());
+
+        final EditTextPreference pass = (EditTextPreference)
+                findPreference("password");
+        pass.setOnPreferenceChangeListener(
+                new OnPreferenceChangeListener() {
+                    
+                    @Override
+                    public boolean onPreferenceChange(Preference p, Object o) {
+                        String word = "";
+                        for (int i = 0; i < o.toString().length(); i++) {
+                            word += "*";
+                        }
+                        p.setSummary(word);
+                        return true;
+                    }
+                });
+        pass.getOnPreferenceChangeListener()
+                .onPreferenceChange(pass,
+                (pass.getText() != null)? pass.getText() : "***");
+    }
+}
