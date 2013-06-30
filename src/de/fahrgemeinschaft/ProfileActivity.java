d79b798e35835e6cf9c06c5753d131e2c516f235
orangeman <flo@teleportr.org>
2013-06-30 15:27:59 +0200

    refactor profile activity

--- src/de/fahrgemeinschaft/ProfileActivity.java
+++ src/de/fahrgemeinschaft/ProfileActivity.java
@@ -7,57 +7,56 @@
 
 package de.fahrgemeinschaft;
 
+import org.teleportr.ConnectorService;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
-public class ProfileActivity extends SherlockPreferenceActivity {
+public class ProfileActivity extends SherlockPreferenceActivity
+        implements OnSharedPreferenceChangeListener {
+
+    private SharedPreferences prefs;
 
     @Override
     @SuppressWarnings("deprecation")
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // in case the background service isn't running anyway
+        startService(new Intent(this, ConnectorService.class));
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(this);
 
         addPreferencesFromResource(R.xml.profile);
+        setSummaries();
+    }
 
-        final EditTextPreference user = (EditTextPreference)
-                findPreference("username");
-        user.setOnPreferenceChangeListener(
-                new OnPreferenceChangeListener() {
-                    
-                    @Override
-                    public boolean onPreferenceChange(Preference p, Object o) {
-                        p.setSummary(o.toString());
-                        return true;
-                    }
-                });
-        user.setSummary(user.getText());
+    private void setSummaries() {
+        ((EditTextPreference) findPreference("username"))
+            .setSummary(prefs.getString("username", ""));
 
-        final EditTextPreference pass = (EditTextPreference)
-                findPreference("password");
-        pass.setOnPreferenceChangeListener(
-                new OnPreferenceChangeListener() {
-                    
-                    @Override
-                    public boolean onPreferenceChange(Preference p, Object o) {
-                        String word = "";
-                        Crouton.makeText(ProfileActivity.this, "logging in...",
-                                Style.INFO).show();
-                        for (int i = 0; i < o.toString().length(); i++) {
-                            word += "*";
-                        }
-                        p.setSummary(word);
-                        return true;
-                    }
-                });
-        pass.getOnPreferenceChangeListener()
-                .onPreferenceChange(pass,
-                (pass.getText() != null)? pass.getText() : "***");
+        String word = "";
+        String p = prefs.getString("password", "");
+        for (int i = 0; i < p.toString().length(); i++) word += "*";
+        ((EditTextPreference) findPreference("password")).setSummary(word);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        Crouton.makeText(ProfileActivity.this, "saved "+key, Style.INFO).show();
+        setSummaries();
     }
 }
