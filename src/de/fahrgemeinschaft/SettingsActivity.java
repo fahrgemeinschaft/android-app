ae7c0eb5b0fc01c276057a0f9f883872dbe97871
orangeman <flo@teleportr.org>
2013-07-05 19:07:32 +0200

    clear cache on radius settings change

--- src/de/fahrgemeinschaft/SettingsActivity.java
+++ src/de/fahrgemeinschaft/SettingsActivity.java
@@ -7,35 +7,47 @@
 
 package de.fahrgemeinschaft;
 
+import org.teleportr.ConnectorService;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
-public class SettingsActivity extends SherlockPreferenceActivity {
+public class SettingsActivity extends SherlockPreferenceActivity
+        implements OnSharedPreferenceChangeListener {
+
+    private SharedPreferences prefs;
 
     @Override
     @SuppressWarnings("deprecation")
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         addPreferencesFromResource(R.xml.settings);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
 
-        final ListPreference list = (ListPreference) findPreference("refresh");
-        list.setOnPreferenceChangeListener(
-                new OnPreferenceChangeListener() {
-                    
-                    @Override
-                    public boolean onPreferenceChange(Preference p, Object o) {
-                        p.setSummary(getResources().getString(
-                                R.string.refresh_description, list.getEntries()
-                                        [list.findIndexOfValue((String) o)]));
-                        return true;
-                    }
-                });
-        list.getOnPreferenceChangeListener()
-        .onPreferenceChange(list, list.getValue());
+    void setSummaries() {
+        ListPreference list = (ListPreference) findPreference("refresh");
+        list.setSummary(getResources().getString(
+                R.string.refresh_description, list.getEntry()));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals("radius_from") || key.equals("radius_to")) {
+            getContentResolver().delete(Uri.parse(
+                    "content://de.fahrgemeinschaft/rides"), null, null);
+            startService(new Intent(this, ConnectorService.class)
+                    .setAction(ConnectorService.SEARCH));
+        }
     }
 }
