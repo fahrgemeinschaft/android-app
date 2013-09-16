525dc816e7ac668f954fb1764af7ad88638e1536
orangeman <flo@teleportr.org>
2013-09-17 00:58:25 +0200

    second place text row

--- src/de/fahrgemeinschaft/util/PlaceImageButton.java
+++ src/de/fahrgemeinschaft/util/PlaceImageButton.java
@@ -7,31 +7,43 @@
 
 package de.fahrgemeinschaft.util;
 
+import org.teleportr.Place;
+
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.Button;
+import android.widget.TextView;
 import de.fahrgemeinschaft.R;
 
 public class PlaceImageButton extends BaseImageButton {
 
-    public Button btn;
+    public TextView name;
+    private TextView address;
 
     @Override
     protected int inflate() {
-        return R.layout.btn_date_picker;
+        return R.layout.btn_place_picker;
     }
 
     public PlaceImageButton(Context context) {
         super(context);
-        btn = (Button) findViewById(R.id.text);
-        btn.setId(ID--);
+        name = (TextView) findViewById(R.id.name);
+        name.setId(ID--);
+        address = (TextView) findViewById(R.id.address);
+        address.setId(ID--);
     }
 
     public PlaceImageButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        btn = (Button) findViewById(R.id.text);
-        btn.setId(ID--);
-        btn.setText(getContext().getString(attrs.getAttributeResourceValue(
+        name = (TextView) findViewById(R.id.name);
+        name.setId(ID--);
+        name.setText(getContext().getString(attrs.getAttributeResourceValue(
                 android, "text", R.string.app_name)));
+        address = (TextView) findViewById(R.id.address);
+        address.setId(ID--);
+    }
+
+    public void setPlace(Place place) {
+        name.setText(place.getName());
+        address.setText(place.getAddress());
     }
 }
