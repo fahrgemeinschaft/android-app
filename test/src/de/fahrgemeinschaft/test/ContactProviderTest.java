e9e44894f317d2f61e38b8c77d6b29d48e5b1aa8
orangeman <flo@teleportr.org>
2013-09-25 19:00:39 +0200

    insert contacts

--- test/src/de/fahrgemeinschaft/test/ContactProviderTest.java
+++ test/src/de/fahrgemeinschaft/test/ContactProviderTest.java
@@ -1,6 +1,8 @@
 package de.fahrgemeinschaft.test;
 
 import de.fahrgemeinschaft.ContactProvider;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.test.ProviderTestCase2;
 
 public class ContactProviderTest extends ProviderTestCase2<ContactProvider> {
@@ -9,4 +11,26 @@ public class ContactProviderTest extends ProviderTestCase2<ContactProvider> {
         super(ContactProvider.class, "de.fahrgemeinschaft.test");
     }
 
+    @Override
+    protected void setUp() throws Exception {
+        ContentValues cv = new ContentValues();
+        cv.put("mail", "blablamail@gmx.net");
+        cv.put("mobile", "123456789");
+        cv.put("user", "foo");
+        getMockContentResolver().insert(Uri.parse(
+                "content://de.fahrgemeinschaft.test/contacts"), cv);
+        super.setUp();
+    }
+
+    public void testGetMails() {
+        getMockContentResolver().query(Uri.parse(
+                "content://de.fahrgemeinschaft.test/users/foo/mails"),
+                null, null, null, null);
+        // query mails
+        // assert sort order
+    }
+
+    public void testStoreContactsBeforeLogin() {
+        // reassign all empty contacts to now logged in user..
+    }
 }
