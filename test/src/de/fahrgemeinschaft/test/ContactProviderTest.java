29122d683fdf5fe6f6acd9de7b28594889f0d266
MoritzC <moritz@sonnenstreifen.de>
2013-09-25 22:57:03 +0200

    tests ready

--- test/src/de/fahrgemeinschaft/test/ContactProviderTest.java
+++ test/src/de/fahrgemeinschaft/test/ContactProviderTest.java
@@ -2,6 +2,7 @@ package de.fahrgemeinschaft.test;
 
 import de.fahrgemeinschaft.ContactProvider;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.test.ProviderTestCase2;
 
@@ -14,6 +15,16 @@ public class ContactProviderTest extends ProviderTestCase2<ContactProvider> {
     @Override
     protected void setUp() throws Exception {
         ContentValues cv = new ContentValues();
+        cv.put("mail", "foo@bar.net");
+        cv.put("mobile", "123456789");
+        cv.put("user", "foo");
+        getMockContentResolver().insert(Uri.parse(
+                "content://de.fahrgemeinschaft.test/contacts"), cv);
+        cv.put("mail", "blablamail@gmx.net");
+        cv.put("mobile", "123456789");
+        cv.put("user", "foo");
+        getMockContentResolver().insert(Uri.parse(
+                "content://de.fahrgemeinschaft.test/contacts"), cv);
         cv.put("mail", "blablamail@gmx.net");
         cv.put("mobile", "123456789");
         cv.put("user", "foo");
@@ -23,14 +34,42 @@ public class ContactProviderTest extends ProviderTestCase2<ContactProvider> {
     }
 
     public void testGetMails() {
-        getMockContentResolver().query(Uri.parse(
+        Cursor mails = getMockContentResolver().query(Uri.parse(
                 "content://de.fahrgemeinschaft.test/users/foo/mails"),
                 null, null, null, null);
-        // query mails
-        // assert sort order
+        assertEquals(2, mails.getCount());
+        mails.moveToFirst();
+        assertEquals("blablamail@gmx.net", mails.getString(1));
+        mails.moveToNext();
+        assertEquals("foo@bar.net", mails.getString(1));
+    }
+
+    public void testDelete() {
+        getMockContentResolver().delete(Uri.parse(
+                "content://de.fahrgemeinschaft.test/users/foo"),
+                null, null);
+        Cursor mails = getMockContentResolver().query(Uri.parse(
+                "content://de.fahrgemeinschaft.test/users/foo/mails"),
+                null, null, null, null);
+        assertEquals(0, mails.getCount());
     }
 
     public void testStoreContactsBeforeLogin() {
+        ContentValues cv = new ContentValues();
+        cv.put("mail", "fg@sonnenstreifen.de");
+        cv.put("mobile", "123456789");
+        getMockContentResolver().insert(Uri.parse(
+                "content://de.fahrgemeinschaft.test/contacts"), cv);
         // reassign all empty contacts to now logged in user..
+        cv.clear();
+        cv.put("user", "bar");
+        getMockContentResolver().update(Uri.parse(
+                "content://de.fahrgemeinschaft.test/contacts"), cv, null, null);
+        Cursor mails = getMockContentResolver().query(Uri.parse(
+                "content://de.fahrgemeinschaft.test/users/bar/mails"),
+                null, null, null, null);
+        assertEquals(1, mails.getCount());
+        mails.moveToFirst();
+        assertEquals("fg@sonnenstreifen.de", mails.getString(1));
     }
 }
