2bc4b88afe44928bdb829cf2d040f8ac2e83ea9b
orangeman <flo@teleportr.org>
2014-10-15 23:00:04 +0200

    streifenhoernchen only for deactivated rides

--- src/main/java/de/fahrgemeinschaft/RideDetailsFragment.java
+++ src/main/java/de/fahrgemeinschaft/RideDetailsFragment.java
@@ -181,8 +181,7 @@ public class RideDetailsFragment extends SherlockFragment
                 view.reg_date.setVisibility(View.GONE);
 
                 if (isMyRide(cursor)) {
-                    if ((isFuture(cursor.getLong(COLUMNS.DEPARTURE))
-                            && isActive(cursor)) || isReoccuring(cursor)) {
+                    if (isActive(cursor)) {
                         view.streifenhoernchen.setVisibility(View.GONE);
                     } else {
                         view.streifenhoernchen.setVisibility(View.VISIBLE);
